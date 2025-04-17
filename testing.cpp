d0490673ad04750c19b333733dbca9bbf2215b98
Harshkaushik04 <142307211+Harshkaushik04@users.noreply.github.com>
2025-04-17 19:58:46 +0530

    Add files via upload

---
+++ testing.cpp
@@ -0,0 +1,19 @@
+#include "headers/agent.hpp"
+#include "headers/cube.hpp"
+#include "headers/model.hpp"
+#include<iostream>
+#include<iostream>
+using namespace std;
+
+int main(){
+    torch::DeviceType device=torch::kCUDA;
+    cout<<torch::cuda::is_available()<<endl;
+    Cube cube=Cube();
+    Env env=Env(cube,Stage::Cross1,0,0);
+    CubeNetImpl model=CubeNetImpl(0.0001,0.2,16);
+    model.loadModel("/home/harsh/RL/Cube/cross_training2/model_itter/330001.pt");
+    model.to(device);
+    Agent agent=Agent(model,device,device,0,0,5000,16,100000,128,0.0001,0.99,env);
+    agent.test(env);
+    return 0;
+}

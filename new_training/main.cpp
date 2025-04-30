86ee3d4051a8cb6dd02448f03fb64c95e0e06334
Harsh <2023aib1006@iitrpr.ac.in>
2025-04-30 19:27:12 +0530

    just changed the epoch number in main.cpp

--- new_training/main.cpp
+++ new_training/main.cpp
@@ -7,8 +7,8 @@ int main(){
     Cube cube=Cube();
     Env env=Env(cube,Stage::Cross1,0,0);
     CubeNetImpl model=CubeNetImpl(0.0001,0.2,16);
-    // model.loadModel("weights/best.pt");
+    model.loadModel("weights/best.pt");
     model.to(device);
     Agent agent=Agent(model,device,device,1,0.1,5000,16,100000,128,0.0001,0.99,env);
-    agent.train(env,10000000,0);
+    agent.train(env,10000000,15000);
 }
